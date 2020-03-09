74a98b305a56af604ab8e7819499cfc4cdb95fcd
jfcontart <jfcontart@idemobi.com>
2020-03-09 22:17:38 +0100

    [STEP] script build in spite of « failed » error at end

--- SQLiteBuilt.sh
+++ SQLiteBuilt.sh
@@ -14,25 +14,32 @@ VERSION=$1
 #prepare dir to compile
 
 cd ./
-rm -r ./sqlite
-mkdir ./${VERSION}
 
 #Download sources files from SQLCipher
 
 curl -OL https://www.sqlite.org/2020/sqlite-autoconf-${VERSION}.tar.gz
-tar -xvf ./sqlite-autoconf-${VERSION}.tar.gz ./sqlite3
+tar -xvf ./sqlite-autoconf-${VERSION}.tar.gz
+rm -r ./sqlite/*
+mkdir ./${VERSION}
+mv ./sqlite-autoconf-${VERSION}/sqlite3.c ./sqlite/sqlite3.c
+mv ./sqlite-autoconf-${VERSION}/sqlite3.h ./sqlite/sqlite3.h
+mv ./sqlite-autoconf-${VERSION}/sqlite3ext.h ./sqlite/sqlite3ext.h
+rm ./sqlite-autoconf-${VERSION}.tar.gz
+rm -r ./sqlite-autoconf-${VERSION}/*
+rm -r ./sqlite-autoconf-${VERSION}
 
 #Compile
 
-xcodebuild -target sqlite_builder -project sqlite3.xcodeproj
+xcodebuild -target "sqlite_builder" -configuration "Release"
 
 #copy result
 
-#cp ./tmp/${VERSION}/sqlite-autoconf-${VERSION}/.libs/libsqlite3.0.dylib ./${VERSION}/
-#open ./${VERSION}
+cp ./delivery/IOS/Release_sqlite3/sqlite3Library/iPhoneUniversal/libsqlite3.a ./${VERSION}/libsqlite3.a
+open ./${VERSION}
 
 #Clean 
 
-#rm -r ./sqlite/*
+rm -r ./delivery/*
+rm -r ./delivery
 
 
